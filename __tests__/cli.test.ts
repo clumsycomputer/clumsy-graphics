c20a987b22605ea9e2367098a15bba6844d7a04b
jmath <clumsycomputer@gmail.com>
2021-11-18 17:54:15 -0700

    feature(source/startAnimationDevelopment): improve tests

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -1,19 +1,26 @@
 import ChildProcess from 'child_process'
 import FileSystem from 'fs'
+import { test, expect } from '@playwright/test'
 
-beforeAll(() => {
-  FileSystem.rmSync('./test-temp', {
-    recursive: true,
-    force: true,
+test.describe('graphics-renderer', () => {
+  const testAnimationModulePath = './__tests__/test-project/Foo.animation.tsx'
+  test.beforeAll(() => {
+    FileSystem.rmSync('./test-temp', {
+      recursive: true,
+      force: true,
+    })
+    FileSystem.mkdirSync('./test-temp')
   })
-  FileSystem.mkdirSync('./test-temp')
-})
-
-describe('graphics-renderer', () => {
-  describe('renderAnimation', () => {
+  test.afterAll(() => {
+    FileSystem.rmSync('./test-temp', {
+      recursive: true,
+      force: true,
+    })
+  })
+  test.describe('renderAnimation', () => {
     test('renderAnimation outputs expected "foo.mp4"', () => {
       ChildProcess.execSync(
-        'graphics-renderer renderAnimation --animationModulePath=./example-project/Foo.animation.tsx --animationMp4OutputPath=./test-temp/foo.mp4 --numberOfFrameRendererWorkers=7'
+        `graphics-renderer renderAnimation --animationModulePath=${testAnimationModulePath} --animationMp4OutputPath=./test-temp/foo.mp4 --numberOfFrameRendererWorkers=7`
       )
       expect(
         ChildProcess.spawnSync(
@@ -21,23 +28,23 @@ describe('graphics-renderer', () => {
           ['-show_frames', './test-temp/foo.mp4'],
           { stdio: 'pipe' }
         ).stdout.toString()
-      ).toMatchSnapshot()
+      ).toMatchSnapshot('animationMp4FramesSnapshot')
     })
   })
-  describe('renderAnimationFrame', () => {
+  test.describe('renderAnimationFrame', () => {
     test('renderAnimationFrame outputs expected "foo_0.svg"', () => {
       ChildProcess.execSync(
-        'graphics-renderer renderAnimationFrame --animationModulePath=./example-project/Foo.animation.tsx --frameFileOutputPath=./test-temp/foo_0.svg --frameIndex=0'
+        `graphics-renderer renderAnimationFrame --animationModulePath=${testAnimationModulePath} --frameFileOutputPath=./test-temp/foo_0.svg --frameIndex=0`
       )
       expect(
         FileSystem.readFileSync('./test-temp/foo_0.svg')
           .toString()
           .replace(/sodipodi:docname="(.)+\.svg"/m, '')
-      ).toMatchSnapshot()
+      ).toMatchSnapshot('frameSvgSnapshot')
     })
     test('renderAnimationFrame outputs expected "foo_0.png"', () => {
       ChildProcess.execSync(
-        'graphics-renderer renderAnimationFrame --animationModulePath=./example-project/Foo.animation.tsx --frameFileOutputPath=./test-temp/foo_0.png --frameIndex=0'
+        `graphics-renderer renderAnimationFrame --animationModulePath=${testAnimationModulePath} --frameFileOutputPath=./test-temp/foo_0.png --frameIndex=0`
       )
       expect(
         ChildProcess.spawnSync(
@@ -45,13 +52,13 @@ describe('graphics-renderer', () => {
           ['-show_frames', './test-temp/foo_0.png'],
           { stdio: 'pipe' }
         ).stdout.toString()
-      ).toMatchSnapshot()
+      ).toMatchSnapshot('framePngFramesSnapshot')
     })
   })
-  describe('convertAnimationToGif', () => {
+  test.describe('convertAnimationToGif', () => {
     test('convertAnimationToGif outputs expected "foo.gif"', () => {
       ChildProcess.execSync(
-        'graphics-renderer renderAnimation --animationModulePath=./example-project/Foo.animation.tsx --animationMp4OutputPath=./test-temp/foo.mp4'
+        `graphics-renderer renderAnimation --animationModulePath=${testAnimationModulePath} --animationMp4OutputPath=./test-temp/foo.mp4`
       )
       ChildProcess.execSync(
         'graphics-renderer convertAnimationToGif --animationMp4SourcePath=./test-temp/foo.mp4 --animationGifOutputPath=./test-temp/foo.gif --gifAspectRatioWidth=512'
@@ -62,14 +69,42 @@ describe('graphics-renderer', () => {
           ['-show_frames', './test-temp/foo.gif'],
           { stdio: 'pipe' }
         ).stdout.toString()
-      ).toMatchSnapshot()
+      ).toMatchSnapshot('animationGifFramesSnapshot')
     })
   })
-})
-
-afterAll(() => {
-  FileSystem.rmSync('./test-temp', {
-    recursive: true,
-    force: true,
+  test.describe('startAnimationDevelopment', () => {
+    let spawnedDevelopmentProcess: ChildProcess.ChildProcess | null = null
+    test.beforeAll(async () => {
+      spawnedDevelopmentProcess = ChildProcess.spawn('graphics-renderer', [
+        'startDevelopment',
+        `--animationModulePath=${testAnimationModulePath}`,
+      ])
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
+    })
+    test.afterAll(() => {
+      spawnedDevelopmentProcess!.kill('SIGINT')
+    })
+    test('startAnimationDevelopment renders and serves animation asset', async ({
+      page,
+    }) => {
+      await page.goto('localhost:3000/?assetType=mp4')
+      const assetResponse = await page.waitForResponse('**/asset/0.mp4')
+      expect(await assetResponse.headerValues('Content-Type')).toEqual([
+        'video/mp4',
+      ])
+    })
+    test('startAnimationDevelopment renders and serves frame asset', async ({
+      page,
+    }) => {
+      await page.goto('localhost:3000/?assetType=png&frameIndex=0')
+      const assetResponse = await page.waitForResponse('**/asset/0_0.png')
+      expect(await assetResponse.headerValues('Content-Type')).toEqual([
+        'image/png',
+      ])
+    })
   })
 })
---
+++ __tests__/test-project/Foo.animation.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { AnimationModule } from '../../source/models/AnimationModule'
+
+const FooAnimationModule: AnimationModule = {
+  animationName: 'foo',
+  frameSize: 512,
+  frameCount: 10,
+  animationSettings: {
+    frameRate: 5,
+    constantRateFactor: 1,
+  },
+  FrameDescriptor: FooFrame,
+}
+
+export default FooAnimationModule
+
+interface FooFrameProps {
+  frameCount: number
+  frameIndex: number
+}
+
+function FooFrame(props: FooFrameProps) {
+  const { frameCount, frameIndex } = props
+  const centerAngle = ((2 * Math.PI) / frameCount) * frameIndex
+  return (
+    <svg viewBox={`0 0 100 100`}>
+      <rect x={0} y={0} width={100} height={100} fill={'black'} />
+      <circle
+        cx={15 * Math.cos(centerAngle) + 50}
+        cy={15 * Math.sin(centerAngle) + 50}
+        r={5}
+        fill={`rgb(${
+          255 * Math.abs(Math.sin(2 * Math.PI * (frameIndex / frameCount)))
+        },128,64)`}
+      />
+    </svg>
+  )
+}

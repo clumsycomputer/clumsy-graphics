849fda475eae7f6e1bf684052b7d3b0aac601f3f
jmath <clumsycomputer@gmail.com>
2022-05-15 11:28:35 -0600

    chore(graphics-renderer): moar release cleanup

--- source/startAnimationDevelopment/browser/index.tsx
+++ source/startAnimationDevelopment/browser/index.tsx
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from '@material-ui/core'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
 import ReactDom from 'react-dom'
 import {
